587064e04d537023f0ab805b0b8479474d906ac3
MinaTough <mina.toughrani@gmail.com>
2021-07-22 19:05:47 +0200

    solvedlab

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require('dotenv/config');
+require("dotenv/config");
 
 // ℹ️ Connects to the database
-require('./db');
+require("./db");
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require('express');
+const express = require("express");
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require('hbs');
+const hbs = require("hbs");
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+require("./config")(app);
 
 // default value for title local
-const projectName = 'lab-express-cinema';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const projectName = "lab-express-cinema";
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
+const index = require("./routes/index");
+app.use("/", index);
+const path = require("path");
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res, next) => res.render("index"));
+app.get("/error", (req, res, next) => resp.render("error"));
+app.get("/not-found", (req, res, next) => resp.render("not-found"));
+app.get("/movies", (req,res,next)=> resp.render("movies"));
+
+app.listen(3000);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app);
+require("./error-handling")(app);
 
 module.exports = app;
